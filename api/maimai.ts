bd3d0a30693df312147ce6eb4e37b785bc6d317f
kkchara <minikyding@gmail.com>
2024-12-15 01:59:55 +0800

    vercel 0.0.9

--- api/maimai.ts
+++ api/maimai.ts
@@ -148,7 +148,7 @@ export default async function handler(
 
 
 
-        <rect data-testid="card-bg" x="0.5" y="0.5" rx="4.5" height="99%" stroke="#e4e2e2" width="466" fill="#151515"
+        <rect data-testid="card-bg" x="0.5" y="0.5" rx="4.5" height="99%" stroke="" width="466" fill="#151515"
             stroke-opacity="1" />
 
 
